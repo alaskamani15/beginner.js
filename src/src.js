5eaecd385d71e2f31e9a71ca1d8f132b131342c2
alaskamani15 <academyofalaska@gmail.com>
2015-11-23 23:22:09 -0600

    cleaned src.js

--- src/src.js
+++ src/src.js
@@ -11,25 +11,3 @@ var Library = {
     */
 
 }
-
-
-
-(function(window){
-    //I recommend this
-    'use strict';
-    function define_library(){
-        var Library = {};
-        var name = "Timmy";
-        Library.greet = function(){
-            alert("Hello from the " + name + " library.");
-        }
-        return Library;
-    }
-    //define globally if it doesn't already exist
-    if(typeof(Library) === 'undefined'){
-        window.Library = define_Library();
-    }
-    else{
-        console.log("Library already defined.");
-    }
-})(window);
