600df5d2f48c190a948cbee6c1d668db99498096
alaskamani15 <academyofalaska@gmail.com>
2015-11-23 23:28:21 -0600

    Clean and ADD ++ (stop)
    stop == alert

--- src/src.js
+++ src/src.js
@@ -1,9 +1,17 @@
+//O. M. J. This actually works. YAY!!!
+
 console.log("Running!");
 
 var Library = {
-    name: "Timmy", //Library has been called Timmy
+    name: "John", //Library has been called Timmy
     greet: function(){
         alert("Hello from the " + Library.name + " library.");
+        console.log("User executed Library.greet()!" + "Hi " + name);
+    }
+    stop: function(text){
+        var alerttext = this.text;
+        alert(alerttext);
+        console.log("You just alerted" + alerttext);
     }
     /*
     This library is what is known as an object literal. To invoke the greet function, we would write:
@@ -11,3 +19,5 @@ var Library = {
     */
 
 }
+
+console.log("All code completed with no errors. \n\n\n Success!")
