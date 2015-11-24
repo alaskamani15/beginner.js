d5f4663c323b682176b548d7764f0b645427372e
alaskamani15 <academyofalaska@gmail.com>
2015-11-24 17:56:35 -0600

    ????

--- src/src.js
+++ src/src.js
@@ -3,21 +3,24 @@
 console.log("Running!");
 
 var Library = {
-    name: "John", //Library has been called Timmy
+    name: "Timmy", //Library has been called Timmy
     greet: function(){
         alert("Hello from the " + Library.name + " library.");
         console.log("User executed Library.greet()!" + "Hi " + name);
     }
-    stop: function (text){
-        var alerttext = text;
-        alert(alerttext);
-        console.log("You just alerted" + alerttext);
-    }
+    
     /*
     This library is what is known as an object literal. To invoke the greet function, we would write:
     Library.greet();
     */
 
 }
+
+function stop(text){
+        var alerttext = text;
+        alert(alerttext);
+        console.log("You just alerted" + alerttext);
+    }
+
 
 console.log("All code completed with no errors. \n\n\n Success!")
