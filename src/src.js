258c2661b98eab0bdc65487c795b0252a699b967
alaskamani15 <academyofalaska@gmail.com>
2015-11-23 23:00:33 -0600

    src.js update

--- src/src.js
+++ src/src.js
@@ -1,1 +1,12 @@
-//source of js ---
+var Library = {
+    name: "Timmy", //Library has been called Timmy
+    greet: function(){
+        alert("Hello from the " + Library.name + " library.");
+    }
+    /*
+    This library is what is known as an object literal. To invoke the greet function, we would write:
+    Library.greet();
+    */
+
+}
+
